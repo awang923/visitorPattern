ea05cdcfc5252d1ae2f9e45ac844f2dbde16012b
jsegu447 <jsegu010@ucr.edu>
2020-11-30 23:09:25 -0800

    fix visitor

--- visitor.hpp
+++ visitor.hpp
@@ -14,20 +14,20 @@ class CountVisitor{
     public:
         CountVisitor(): ops(0),rands(0),mults(0),divs(0),add(0),sub(0),pow(0){}
 
-        void visit_op();
-        int op_count(){ops++;}
-        void visit_rand();
-        int rand_count(){rands++;}
-        void visit_mult();
-        int mult_count(){mults++;}
-        void visit_div();
-        int div_count(){divs++;}
-        void visit_add();
-        int add_count(){add++;}
-        void visit_sub();
-        int sub_count(){sub++}
-        void visit_pow();
-        int pow_count(){pow++}
+        void visit_op(){ops++;}
+        int op_count(){return ops;}
+        void visit_rand(){rands++;}
+        int rand_count(){return rands;}
+        void visit_mult(){mults++;}
+        int mult_count(){return mults;}
+        void visit_div(){divs++;}
+        int div_count(){return divs;}
+        void visit_add(){add++;}
+        int add_count(){return add;}
+        void visit_sub(){sub++;}
+        int sub_count(){return sub;}
+        void visit_pow(){pow++;}
+        int pow_count(){return pow;}
 
 };
 
